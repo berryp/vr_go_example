9dc7fb5fd49492204c1242da39b9654148c545e4
Berry Phillips <berryphillips@gmail.com>
2013-01-02 12:15:12 -0800

    Adding source files.

---
+++ src/hello/web.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"net/http"
+	"os"
+)
+
+var port string
+
+func HelloHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintln(w, "Hello, World!")
+}
+
+func main() {
+	flag.StringVar(&port, "port", "5000", "HTTP Server port")
+	flag.Parse()
+
+	if os.Getenv("PORT") != "" {
+		port = os.Getenv("PORT")
+	}
+
+	http.HandleFunc("/", HelloHandler)
+
+	fmt.Printf("Listening on %s\n", port)
+	err := http.ListenAndServe(":"+port, nil)
+	if err != nil {
+		panic(err)
+	}
+}
